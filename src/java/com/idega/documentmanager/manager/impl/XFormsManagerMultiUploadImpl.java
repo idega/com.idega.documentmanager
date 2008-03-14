0c8056f046ac02543145a384eec0fd7788fb35fc
Arunas <arunas@idega.com>
2008-03-14 12:09:36 +0000

    *** empty log message ***

--- src/java/com/idega/documentmanager/manager/impl/XFormsManagerMultiUploadImpl.java
+++ src/java/com/idega/documentmanager/manager/impl/XFormsManagerMultiUploadImpl.java
@@ -12,39 +12,29 @@ import com.idega.util.xml.XPathUtil;
 
 /**
  * @author <a href="mailto:arunas@idega.com">Arūnas Vasmanas</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  *
- * Last modified: $Date: 2008/03/07 13:44:15 $ by $Author: civilis $
+ * Last modified: $Date: 2008/03/14 12:10:03 $ by $Author: arunas $
  */
 public class XFormsManagerMultiUploadImpl extends XFormsManagerImpl implements XFormsManagerMultiUpload{
 	
 
-	private static final String upload_data_source = "_upd";
+	private static final String INSERT_TAG = "./descendant::xf:insert[@at='last()']";
+	private static final String REPEAT_TAG ="./descendant::xf:repeat[@id='upload-entries']";
+	private static final String DELETE_TAG="./descendant::xf:delete";
+	private static final String INSTANCE = "instance('data-instance')/Add_One_";
+	private static final String OUTPUT="./descendant::xf:output";
+	private static final String OUTPUT_VALUE_BEGIN="count(preceding-sibling::Add_One_";
+	private static final String OUTPUT_VALUE_END=") + 1";
+	private static final String BIND = "bind.";
 	@Override
 	protected ComponentDataBean newXFormsComponentDataBeanInstance() {
 		return new ComponentMultiUploadBean();
 	}
 	
 	@Override
-	protected void loadXFormsComponentDataBean(FormComponent component, Document xform, Element componentElement) {
-		
+	protected void loadXFormsComponentDataBean(FormComponent component, Document xform, Element componentElement) {	
 		super.loadXFormsComponentDataBean(component, xform, componentElement);
-		
-		ComponentMultiUploadBean xfMultiUploadBean = (ComponentMultiUploadBean)component.getXformsComponentDataBean();
-		
-		componentElement = xfMultiUploadBean.getElement();
-		
-		String componentElementId = componentElement.getAttribute(FormManagerUtil.id_att);
-	
-		Element externalUploadInstance = FormManagerUtil.getElementByIdFromDocument(xform, FormManagerUtil.head_tag, getMultiUploadInstanceIdenfiner(componentElementId));
-		
-		xfMultiUploadBean.setMultiUploadInstance(externalUploadInstance);
-	}
-	
-	
-	private String getMultiUploadInstanceIdenfiner(String element_id) {
-		
-		return element_id+upload_data_source;
 	}
 	
 	@Override
@@ -66,62 +56,46 @@ public class XFormsManagerMultiUploadImpl extends XFormsManagerImpl implements X
 		
 		super.addComponentToDocument(component);
 		
-		Document xforms_document = component.getContext().getXformsXmlDoc();
-		
 		ComponentMultiUploadBean xfMultiUploadComponentBean = (ComponentMultiUploadBean)component.getXformsComponentDataBean();
 		
-		Element new_xforms_element =xfMultiUploadComponentBean.getMultiUploadInstance();
-		
-		Element data_model_instance_element = FormManagerUtil.getElementByIdFromDocument(xforms_document, FormManagerUtil.head_tag, FormManagerUtil.data_mod);
-		
-		if(new_xforms_element != null) {
-					
-			new_xforms_element = (Element)xforms_document.importNode(new_xforms_element, true);
-			new_xforms_element.setAttribute(FormManagerUtil.id_att, getMultiUploadInstanceIdenfiner(component.getId()));
-			new_xforms_element = (Element)data_model_instance_element.appendChild(new_xforms_element);
-			xfMultiUploadComponentBean.setMultiUploadInstance(new_xforms_element);
-			
-			
-			XPathUtil util = new XPathUtil("./descendant::xf:insert[@at='last()']");
+			XPathUtil util = new XPathUtil(INSERT_TAG);
 			Element insert = (Element)util.getNode(xfMultiUploadComponentBean.getElement());
 			insert.setAttribute(FormManagerUtil.nodeset_att, constructInsertNodeset(component.getId()));
-			insert.setAttribute(FormManagerUtil.origin_att, constructInsertOrigin(component.getId()));
 		
-			util = new XPathUtil("./descendant::xf:repeat[@id='data_repeat']");
+			util = new XPathUtil(REPEAT_TAG);
+			insert = (Element)util.getNode(xfMultiUploadComponentBean.getElement());
+			insert.setAttribute(FormManagerUtil.bind_att, constuructRepeatdBind(component.getId()));
+				
+			util = new XPathUtil(DELETE_TAG);
 			insert = (Element)util.getNode(xfMultiUploadComponentBean.getElement());
 			insert.setAttribute(FormManagerUtil.nodeset_att, constructInsertNodeset(component.getId()));
-		
-			util = new XPathUtil("./descendant::xf:upload");
+			
+			util = new XPathUtil(OUTPUT);
 			insert = (Element)util.getNode(xfMultiUploadComponentBean.getElement());
-			insert.setAttribute(FormManagerUtil.bind_att, constuructUploadBind(component.getId()));
-		
-			util = new XPathUtil("./descendant::xf:delete");
-			insert = (Element)util.getNode(xfMultiUploadComponentBean.getElement());
-			insert.setAttribute(FormManagerUtil.nodeset_att, constructInsertNodeset(component.getId()));
-		
-		}
+			insert.setAttribute(FormManagerUtil.value_att, constructOutputValue(component.getId()));
+
 	}
 	
 	private String constructInsertNodeset(String component_id) {
 		
 		StringBuffer buf = new StringBuffer();		
-		buf.append("instance('data-instance')/Add_One_").append(component_id).append("/attachment");
+		buf.append(INSTANCE).append(component_id);
 		return buf.toString();
 		
 	}
 	
-	private String constructInsertOrigin (String component_id) {
+	private String constuructRepeatdBind (String component_id) {
 		
 		StringBuffer buf = new StringBuffer();
-		buf.append("instance('").append(component_id).append("_upd')");
+		buf.append(BIND).append(component_id);
 		return buf.toString();
 		
 	}
 	
-	private String constuructUploadBind (String component_id) {
+	private String constructOutputValue(String component_id) {
 		
-		StringBuffer buf = new StringBuffer();
-		buf.append("bind.").append(component_id);
+		StringBuffer buf = new StringBuffer();		
+		buf.append(OUTPUT_VALUE_BEGIN).append(component_id).append(OUTPUT_VALUE_END);
 		return buf.toString();
 		
 	}
