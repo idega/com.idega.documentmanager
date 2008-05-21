bcca8cad6c6e63b7d8de53cfcdbe0be7ce7b0e94
Arunas <arunas@idega.com>
2008-05-21 13:18:18 +0000

    setReadonly properties

--- src/java/com/idega/documentmanager/component/impl/FormComponentMultiUploadDescriptionImpl.java
+++ src/java/com/idega/documentmanager/component/impl/FormComponentMultiUploadDescriptionImpl.java
@@ -1,15 +1,19 @@
 package com.idega.documentmanager.component.impl;
 
+import org.w3c.dom.Element;
+
 import com.idega.documentmanager.business.component.ComponentMultiUploadDescription;
 import com.idega.documentmanager.business.component.properties.PropertiesMultiUploadDescription;
 import com.idega.documentmanager.component.properties.impl.ComponentPropertiesMultiUploadDescription;
 import com.idega.documentmanager.component.properties.impl.ConstUpdateType;
 import com.idega.documentmanager.manager.XFormsManagerMultiUploadDescription;
+import com.idega.documentmanager.util.FormManagerUtil;
+import com.idega.util.CoreConstants;
 /**
  * @author <a href="mailto:arunas@idega.com">Arūnas Vasmanas</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  *
- * Last modified: $Date: 2008/05/10 11:49:08 $ by $Author: arunas $
+ * Last modified: $Date: 2008/05/21 13:18:18 $ by $Author: arunas $
  */
 public class FormComponentMultiUploadDescriptionImpl extends FormComponentImpl implements ComponentMultiUploadDescription{
 	
@@ -23,9 +27,20 @@ public class FormComponentMultiUploadDescriptionImpl extends FormComponentImpl i
 	public void setReadonly(boolean readonly) {
 		
 		super.setReadonly(readonly);
+		
+		Element bindElement = this.getXformsComponentDataBean().getBind().getBindElement();
+		Element groupElem = this.getXformsComponentDataBean().getElement();
+		
 		if(readonly) {
-
+		    bindElement.setAttribute(FormManagerUtil.relevant_att, FormManagerUtil.xpath_false);
+		    groupElem.setAttribute(FormManagerUtil.bind_att, bindElement.getAttribute("id"));
 //			TODO: needs to transform to link list for downloading files
+		}else {
+		    
+		    if (!CoreConstants.EMPTY.equals(bindElement.getAttribute(FormManagerUtil.relevant_att)))
+			bindElement.removeAttribute(FormManagerUtil.relevant_att);
+		    	groupElem.removeAttribute(FormManagerUtil.bind_att);
+		    
 		}
 	}
 	
@@ -43,7 +58,7 @@ public class FormComponentMultiUploadDescriptionImpl extends FormComponentImpl i
 	
 	@Override
 	protected void setProperties() {
-	    	
+	
 	    	super.setProperties();
 		ComponentPropertiesMultiUploadDescription properties = (ComponentPropertiesMultiUploadDescription)getProperties();
 		properties.setPlainRemoveButtonLabel(getXFormsManager().getRemoveButtonLabel(this));
