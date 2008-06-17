173c31025c58df138f3289ce7cb8311a4364cdc0
Vytautas Civilis <civilis@idega.com>
2008-06-17 12:16:11 +0000

    saveSubmittedData method added

--- src/java/com/idega/documentmanager/business/PersistenceManager.java
+++ src/java/com/idega/documentmanager/business/PersistenceManager.java
@@ -1,14 +1,16 @@
 package com.idega.documentmanager.business;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import com.idega.documentmanager.component.FormDocument;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  * 
- * Last modified: $Date: 2008/04/10 01:08:00 $ by $Author: civilis $
+ * Last modified: $Date: 2008/06/17 12:16:11 $ by $Author: civilis $
  */
 public interface PersistenceManager {
 
@@ -19,4 +21,6 @@ public interface PersistenceManager {
 	public abstract PersistedFormDocument takeForm(Long formId);
 	
 	public abstract List<PersistedForm> getStandaloneForms();
+	
+	public abstract void saveSubmittedData(Long formId, InputStream is, String identifier) throws IOException;
 }
