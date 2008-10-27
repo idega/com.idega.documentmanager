80129a70e7304c04a1854bd12d0198df37d4da28
Vytautas Civilis <civilis@idega.com>
2008-10-27 20:21:20 +0000

    getAllStandaloneFormsSubmissions added

--- src/java/com/idega/documentmanager/business/PersistenceManager.java
+++ src/java/com/idega/documentmanager/business/PersistenceManager.java
@@ -8,39 +8,51 @@ import com.idega.documentmanager.component.FormDocument;
 
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.8 $
  * 
- * Last modified: $Date: 2008/06/18 13:18:00 $ by $Author: civilis $
+ *          Last modified: $Date: 2008/10/27 20:21:20 $ by $Author: civilis $
  */
 public interface PersistenceManager {
 
 	public abstract PersistedFormDocument loadForm(Long formId);
-	
+
 	public abstract PersistedFormDocument loadPopulatedForm(Long submissionId);
-	
-	public abstract PersistedFormDocument saveForm(FormDocument document) throws IllegalAccessException;
-	
+
+	public abstract PersistedFormDocument saveForm(FormDocument document)
+			throws IllegalAccessException;
+
 	public abstract PersistedFormDocument takeForm(Long formId);
-	
-	public abstract List<PersistedForm> getStandaloneForms();
-	
+
+	public abstract List<Form> getStandaloneForms();
+
 	/**
-	 * @param formId - not null
-	 * @param is - not null
-	 * @param identifier - could be null, would be generated some random identifier
+	 * @param formId
+	 *            - not null
+	 * @param is
+	 *            - not null
+	 * @param identifier
+	 *            - could be null, would be generated some random identifier
 	 * @return submission id
 	 * @throws IOException
 	 */
-	public abstract Long saveSubmittedData(Long formId, InputStream is, String identifier) throws IOException;
-	
+	public abstract Long saveSubmittedData(Long formId, InputStream is,
+			String identifier) throws IOException;
+
 	/**
 	 * 
 	 * @param submissionId
-	 * @param formId - not null
-	 * @param is - not null
-	 * @param identifier - could be null, would be generated some random identifier
+	 * @param formId
+	 *            - not null
+	 * @param is
+	 *            - not null
+	 * @param identifier
+	 *            - could be null, would be generated some random identifier
 	 * @return submission id
 	 * @throws IOException
 	 */
-	public abstract Long saveSubmittedDataByExistingSubmission(Long submissionId, Long formId, InputStream is, String identifier) throws IOException;
+	public abstract Long saveSubmittedDataByExistingSubmission(
+			Long submissionId, Long formId, InputStream is, String identifier)
+			throws IOException;
+
+	public abstract List<Submission> getAllStandaloneFormsSubmissions();
 }
