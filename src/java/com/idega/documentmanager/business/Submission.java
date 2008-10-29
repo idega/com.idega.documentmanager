ca09ed37fb6652a5a9761f1c897a8ac163b25e3c
Vytautas Civilis <civilis@idega.com>
2008-10-29 11:25:09 +0000

    getSubmissionDocument method added

--- src/java/com/idega/documentmanager/business/Submission.java
+++ src/java/com/idega/documentmanager/business/Submission.java
@@ -2,11 +2,13 @@ package com.idega.documentmanager.business;
 
 import java.util.Date;
 
+import org.w3c.dom.Document;
+
 /**
  * @author <a href="mailto:civilis@idega.com">Vytautas Čivilis</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * 
- *          Last modified: $Date: 2008/10/27 20:21:00 $ by $Author: civilis $
+ *          Last modified: $Date: 2008/10/29 11:25:09 $ by $Author: civilis $
  */
 public interface Submission {
 
@@ -23,4 +25,6 @@ public interface Submission {
 	public abstract Form getXform();
 
 	public abstract Boolean getIsFinalSubmission();
+
+	public abstract Document getSubmissionDocument();
 }
